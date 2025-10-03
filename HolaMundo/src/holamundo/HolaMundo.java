1d3d0b58f1eee00e3e7db72c7423c4dc1d3774a3
1DAM - Lucia Elizo Gómez <lelizog01@informatica.iesvalledeljerteplasencia.es>
2025-10-03 15:58:38 +0200

    holaMundo
    subir holamundo

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ * Mi primer programa   
+ * @author Lucia
+ * @since 2025
+ */
+public class HolaMundo {
+
+    /**
+     * Metodo main
+     * @author Lucia
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+        
+        
+    }
+    
+}
