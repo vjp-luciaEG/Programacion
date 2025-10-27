e3427b40eb3c93b3061dc704177167d6619efd15
1DAM - Lucia Elizo Gómez <lelizog01@informatica.iesvalledeljerteplasencia.es>
2025-10-27 23:25:12 +0100

    ej14

--- T4/T4Ej14/src/t4ej14/T4Ej14.java
+++ T4/T4Ej14/src/t4ej14/T4Ej14.java
@@ -16,28 +16,31 @@ public class T4Ej14 {
     public static int pedirDato(){
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduzca un numero: ");
-        int dato = entrada.nextInt();
-        return dato;
+        int dato = entrada.nextInt(); //Entrada dato
+        return dato; //Devolvemos el dato de entrada
     }
     
-    public static int errorDato(int dato){
-        dato=pedirDato();
+    public static boolean errorDato(int dato){
+        boolean estado=false; //Comprobamos el estado del dato de entrada suponiendo que es invalido
         if(dato<=0){
-            System.err.println("Error: OPCION NO DISPONIBLE \n INTRODUCIR DATO SUPERIOR A 0");
+            System.err.println("Error: OPCION NO DISPONIBLE \nINTRODUCIR DATO SUPERIOR A 0");
+            return estado; //Devolvemos estado invalido
         }
-        return dato;
+        else{
+            estado = true;
+            return estado; //Devolvemos estado valido
+        }
     }
     
-    public static void multiplosDe3(int dato ){
-        int num=1;
-        int numMostrados=0;
-        System.out.println("Multiplos de 3: ");
-        while(num>=1 && num<=dato){
-            if(dato%3==0){
-                System.out.println(num);
+    public static void multiplosDe3(int dato){;
+        int numMostrados=0; 
+        System.out.println("Multiplos de 3: "); //Calculo de los multiplos de 3(1-->dato de entrada)
+        
+        for(int i=1;i<=dato;i++){
+            if(i%3==0){
+                System.out.println(i);
                 numMostrados++;
-            }
-            num++;
+            } 
         }
         System.out.println("Numeros totales mostrados: " + numMostrados);
     }
@@ -45,16 +48,14 @@ public class T4Ej14 {
         // TODO code application logic here
         //MULTIPLOS DE 3
         
-        int dato=pedirDato();
+        int dato=pedirDato(); //Pedimos el dato de entrada invocando al metodo
+        boolean estado=errorDato(dato); //Declaramos el estado del dato de entrada invocando al metodo
         
-        do{
-            pedirDato();
-            errorDato(pedirDato());
-        } while(dato<=0);
-        
-        multiplosDe3(dato);
-        
-        
+         while(estado==false){
+             dato=pedirDato();  //Si el estado es invalido, reescribimos el dato de entrada
+             estado=errorDato(dato); //Volvemos a verificar el estado del dato de entrada
+         }
+        multiplosDe3(dato); //Si el estado es valido, se ejecuta el metodo
     }
 
 }
