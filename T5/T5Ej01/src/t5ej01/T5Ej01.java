f0216198c680172351bae8ef5a8f5824b06ec276
1DAM - Lucia Elizo Gómez <lelizog01@informatica.iesvalledeljerteplasencia.es>
2025-11-10 19:59:59 +0100

    completo

--- T5/T5Ej01/src/t5ej01/T5Ej01.java
+++ T5/T5Ej01/src/t5ej01/T5Ej01.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package t5ej01;
-
+import java.util.Scanner;
 /**
  *
  * @author alumno
@@ -17,6 +17,36 @@ public class T5Ej01 {
         // TODO code application logic here
         //
         
+        Coche miCoche = new Coche();
+        Coche cochePadre = new Coche();
+        
+        miCoche.setMarca("BMW");
+        miCoche.setModelo("M4");
+        miCoche.setColor("Negro");
+        
+        cochePadre.setMarca("Audi");
+        cochePadre.setModelo("A4");
+        cochePadre.setColor("Azul");
+        
+        miCoche.arrancarCoche();
+        cochePadre.arrancarCoche();
+        
+        for(int i=1;i<=5;i++){
+            miCoche.acelerarCoche();
+        }
+        
+        miCoche.frenarCoche();
+        miCoche.frenarCoche();
+        
+        cochePadre.acelerarCoche();
+        cochePadre.acelerarCoche();
+        cochePadre.acelerarCoche();
+        
+        cochePadre.apagarCoche();
+        
+        miCoche.obtenerEstado();
+        
+        cochePadre.obtenerEstado();
         
     }
     
